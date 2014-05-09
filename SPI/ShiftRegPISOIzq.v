276faf81b5de6426fd95a1417c0dda8fd30b493e
Jorge <jsequeira03@gmail.com>
2014-05-09 00:26:36 -0700

    Simulaciones y demas

--- SPI/ShiftRegPISOIzq.v
+++ SPI/ShiftRegPISOIzq.v
@@ -1,17 +1,45 @@
 `timescale 1ns / 1ps
 
-module ShiftRegPISOIzq(clk, rst, ena, DatIn, DatOut);
+module ShiftRegPISOIzq(clk, rst, ena, load, DatIn, DatOut);
 
 parameter LARGO = 8;
-input clk, rst, ena;
+input clk, rst, ena, load;
 input [LARGO-1:0]DatIn;
-output DatOut;
-always @(negedge clk, posedge rst)
-	if(rst)
-		DatOut 	= 0;
-	else if(ena)
-		DatOut = {DatOut[LARGO-2:0], DatIn};
-	else
-		DatOut = DatOut;
+output reg DatOut;
+reg [LARGO-2:0]tmp;
+	
+	always @(negedge clk, posedge rst)
 
+		if(rst)
+			DatOut 	<= 0;
+		else if (load)
+			begin
+				tmp <= DatIn[LARGO-2:0];
+				DatOut <= DatIn[LARGO-1];
+			end
+		else if(ena)
+			begin
+				tmp <= {tmp[LARGO-3:0],1'b0};
+				DatOut <= tmp[LARGO-2];
+			end
+		else
+			DatOut <= DatOut;
+	
 endmodule
+
+//
+//   parameter piso_shift = <shift_width>;
+//   
+//   reg [piso_shift-2:0] <reg_name>;
+//   reg                  <output>;
+//
+//   always @(posedge <clock>)
+//      if (<load_signal>) begin
+//         <reg_name> <= <input>[piso_shift-1:1];
+//         <output>    <= <input>[0];
+//      end
+//      else begin
+//         <reg_name> <= {1'b0, <reg_name>[piso_shift-2:1]};
+//         <output>   <= <reg_name>[0];
+//      end
+//					
